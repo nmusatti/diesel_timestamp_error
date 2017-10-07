81077ae5dace16d22ee950e9917753107080aa09
Nicola Musatti <nicola.musatti@gmail.com>
2017-10-07 15:35:47 +0200

    Single file version working with diesel 0.14

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,29 @@ use chrono::prelude::*;
 use diesel::prelude::*;
 use dotenv::dotenv;
 
-pub mod schema;
-pub mod domain;
+pub mod schema {
+	infer_schema!("dotenv:DATABASE_URL");
+}
+
+pub mod domain {
+	use chrono;
+
+	use super::schema::books;
+	
+	#[derive(Queryable, Identifiable, AsChangeset, Clone)]
+	pub struct Book {
+	    pub id : i32,
+	    pub title : Option<String>,
+	    pub save_date : Option<chrono::NaiveDateTime>, 
+	}
+	
+	#[derive(Insertable)]
+	#[table_name="books"]
+	pub struct NewBook<'a> {
+	    pub title : Option<&'a str>,
+	    pub save_date : Option<&'a chrono::NaiveDateTime>, 
+	}
+}
 
 fn main() {
 	dotenv().ok();
